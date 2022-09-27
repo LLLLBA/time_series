c55802995e7263d3589b6704a34782c7f0807c78
LLLLBA <l1348984577@outlook.com>
2022-09-28 00:04:32 +0800

    2020/9/28

--- SMA.py
+++ SMA.py
@@ -65,35 +65,60 @@ eva_series = [166,
               528,
               587,
               ]
+eva_series_test = series + eva_series
 
 
-def SMA(lens, data):
-    temp = []
-    for i in range(lens):
-        temp.append(0)
-    for i in range(lens - 1, len(data)):
-        a = 0
-        for j in range(1, lens + 1):
-            a += data[i - j + 1]
-        a = a / lens
-        temp.append(a)
-    return temp
+def sma(lens, data, far=0):
+    if far == 0:
+        temp_sma = []
+        for i in range(lens):
+            temp_sma.append(0)
+        for i in range(lens - 1, len(data)):
+            a = 0
+            for j in range(1, lens + 1):
+                a += data[i - j + 1]
+            a = a / lens
+            temp_sma.append(a)
+        return temp_sma
+    else:
+        temp_fsma = sma(lens, data)
+        for i in range(len(series), len(series) + far):
+            a = 0
+            for j in range(1, lens + 1):
+                a += temp_fsma[i - j + 1]
+            a = a / lens
+            temp_fsma.append(a)
+        return temp_fsma
 
 
-def SSE(data, series_data, lens):
-    temp = 0
+def sse(data, series_data, lens):
+    temp_sse = 0
     for i in range(lens, len(series_data)):
-        temp += pow(series_data[i] - data[i], 2)
-    return temp
+        temp_sse += pow(series_data[i] - data[i], 2)
+    return temp_sse
 
 
 if __name__ == '__main__':
     temp = []
-
     for i in range(2, len(series) // 2):
-        SMA_1 = SMA(i, series)
+        SMA_1 = sma(i, series)
         SMA_1.pop()
-        temp.append(SSE(SMA_1, series, i))
+        temp.append(sse(SMA_1, series, i))
     temp1 = temp.index(min(temp))
+    for i in range(len(temp)):
+        temp[i] = round(temp[i], 2)
     print(temp)
+    print(temp1 + 2)
     print(temp[temp1])
+
+    temp_test = []
+    for i in range(2, len(series) // 2):
+        SMA_2 = sma(i, series, 31)
+        SMA_2.pop()
+        temp_test.append(sse(SMA_2, eva_series_test, i))
+    temp2 = temp_test.index(min(temp_test))
+    for i in range(len(temp_test)):
+        temp_test[i] = round(temp_test[i], 2)
+    print(temp_test)
+    print(temp2 + 2)
+    print(temp_test[temp2])
