a791098dd877cdca2f544e984486efcc847c6bc3
LLLLBA <l1348984577@outlook.com>
2022-09-28 08:38:21 +0800

    2020/9/28-3

--- SMA.py
+++ SMA.py
@@ -3,122 +3,114 @@
 # Author : 罗浩宇 202004040123
 # File : SMA.py
 # Software: PyCharm
-series = [144,
-          150,
-          175,
-          230,
-          339,
-          296,
-          156,
-          164,
-          144,
-          185,
-          206,
-          315,
-          290,
-          219,
-          211,
-          204,
-          261,
-          285,
-          197,
-          178,
-          169,
-          311,
-          296,
-          305,
-          365,
-          610,
-          598,
-          531,
-          517,
+series = [144.15,
+          149.59,
+          174.57,
+          230.03,
+          339.19,
+          295.54,
+          155.64,
+          163.81,
+          143.64,
+          185.34,
+          206.45,
+          314.64,
+          290.42,
+          219.11,
+          210.59,
+          203.60,
+          261.45,
+          285.38,
+          196.98,
+          178.41,
+          168.82,
+          311.01,
+          296.28,
+          305.10,
+          364.97,
+          610.10,
+          598.44,
+          530.53,
+          517.44,
           ]
-eva_series = [166,
-              169,
-              205,
-              336,
-              301,
-              156,
-              147,
-              71,
-              167,
-              232,
-              317,
-              293,
-              235,
-              247,
-              215,
-              260,
-              274,
-              416,
-              368,
-              300,
-              291,
-              289,
-              302,
-              361,
-              644,
-              599,
-              505,
-              537,
-              529,
-              528,
-              587,
+eva_series = [166.14,
+              168.62,
+              204.81,
+              335.90,
+              301.07,
+              156.26,
+              146.75,
+              71.42,
+              167.21,
+              231.60,
+              316.88,
+              292.89,
+              234.64,
+              247.01,
+              214.70,
+              259.65,
+              274.07,
+              416.43,
+              368.40,
+              300.24,
+              290.88,
+              289.23,
+              302.18,
+              361.27,
+              644.36,
+              598.91,
+              504.83,
+              536.57,
+              528.92,
+              528.18,
+              587.37,
               ]
 eva_series_test = series + eva_series
 
 
-def sma(lens, data, far=0):
-    if far == 0:
-        temp_sma = []
-        for i in range(lens):
-            temp_sma.append(0)
-        for i in range(lens - 1, len(data)):
-            a = 0
-            for j in range(1, lens + 1):
-                a += data[i - j + 1]
-            a = a / lens
-            temp_sma.append(a)
-        return temp_sma
-    else:
-        temp_fsma = sma(lens, data)
-        for i in range(len(series), len(series) + far):
-            a = 0
-            for j in range(1, lens + 1):
-                a += temp_fsma[i - j + 1]
-            a = a / lens
-            temp_fsma.append(a)
-        return temp_fsma
+def sma(lens, data):
+    temp_sma = []
+    for i in range(lens):
+        temp_sma.append(0)
+    for i in range(lens - 1, len(data)):
+        a = 0
+        for j in range(1, lens + 1):
+            a += data[i - j + 1]
+        a = a / lens
+        temp_sma.append(a)
+    return temp_sma
 
 
-def sse(data, series_data, lens):
+def sma_forecast(lens, data, far):
+    cali = sma(lens, data)
+    set = data[(-lens + 1):] + [cali[-1]]
+    for i in range(lens - 1, far):
+        a = 0
+        for j in range(1, lens + 1):
+            a += set[i - j + 1]
+        a = a / lens
+        set.append(a)
+    return set
+
+
+def mse(data, series_data, lens):
     temp_sse = 0
+    count = 0
     for i in range(lens, len(series_data)):
         temp_sse += pow(series_data[i] - data[i], 2)
-    return temp_sse
+        count += 1
+    return temp_sse / count
 
 
 if __name__ == '__main__':
     temp = []
-    for i in range(2, len(series) // 2):
-        SMA_1 = sma(i, series)
+    for i in range(2, len(series)):
+        SMA_1 = sma_forecast(i, series,30)
         SMA_1.pop()
-        temp.append(sse(SMA_1, series, i))
+        temp.append(mse(SMA_1, series, i))
     temp1 = temp.index(min(temp))
     for i in range(len(temp)):
         temp[i] = round(temp[i], 2)
     print(temp)
     print(temp1 + 2)
     print(temp[temp1])
-
-    temp_test = []
-    for i in range(2, len(series) // 2):
-        SMA_2 = sma(i, series, 31)
-        SMA_2.pop()
-        temp_test.append(sse(SMA_2, eva_series_test, i))
-    temp2 = temp_test.index(min(temp_test))
-    for i in range(len(temp_test)):
-        temp_test[i] = round(temp_test[i], 2)
-    print(temp_test)
-    print(temp2 + 2)
-    print(temp_test[temp2])
